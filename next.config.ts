da8f3fd2ede3b09f06e60fffde4e94cc0b117594
biswarup-capsi <biswarup.naha@capsitech.com>
2025-09-05 10:58:42 +0530

    config modified for fixing build conflict

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  GEMINI_API_KEY: process.env.GEMINI_API_KEY
 };
 
 export default nextConfig;
